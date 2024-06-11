9ccb7ad32ea5c4884aa375ef335c7812915a8912
Yebon-Byun <96388412+Yebon-Byun@users.noreply.github.com>
2024-06-11 11:15:10 +0900

    Time: 128 ms (49.88%), Space: 18.1 MB (68.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,16 +1,24 @@
+# MY OWN
+# class Solution:
+#     def moveZeroes(self, nums: List[int]) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         for i in range(len(nums)):
+#             if nums[i] == 0:
+#                 nums.remove(nums[i])
+#                 nums.append(0)
+#             i += 1
+
+# Discuss Section Approach
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+        pos = 0
         
         for i in range(len(nums)):
-            if nums[i] == 0:
-                nums.remove(nums[i])
-                nums.append(0)
-            i += 1
-            
-                
-                
-                
+            el = nums[i]
+            if el != 0:
+                nums[pos], nums[i] = nums[i], nums[pos]
+                pos += 1
+
         
