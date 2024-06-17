9d380fb50a9e562a57bda77caf97979bfe2f2246
Yebon-Byun <96388412+Yebon-Byun@users.noreply.github.com>
2024-06-17 17:06:05 +0900

    Time: 39 ms (73.34%), Space: 16.8 MB (21.00%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -0,0 +1,30 @@
+# MY OWN
+# FAIL, I wasn't sure how to compare values in dictionary.
+# class Solution:
+#     def uniqueOccurrences(self, arr: List[int]) -> bool:
+#         comp = 0
+#         u_arr = set(arr)
+ 
+#         v_arr = []
+#         comp1, comp2 = 0, 0
+#         dict = {i : 0 for i in arr}
+#         for i in arr:
+#             dict[i] += 1
+        
+#         first_value = next(iter(dict.values()))
+#         all_same = all(value == first_value for value in dict.values())
+        
+        
+#         if all_same:
+#             return False
+#         else:
+#             return True
+
+# Approahc 1: Counter
+# Time Comp: O(n)
+# Space Comp: O(n)
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        return len(set(Counter(arr).values())) == len(set(arr))
+            
+            
