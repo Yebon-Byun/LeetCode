8af7710ee52a4e341744344730d404aabc747da3
Yebon-Byun <96388412+Yebon-Byun@users.noreply.github.com>
2024-06-23 18:29:12 +0900

    Time: 34 ms (89.02%), Space: 18.5 MB (89.65%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Approach: Two Pointers
+# Time Comp: O(n)
+# Space Comp: O(1)
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head: return None
+        
+        odd = head
+        even_head = even = head.next
+        
+        while even and even.next:
+            odd.next = odd.next.next
+            odd = odd.next
+            
+            even.next = even.next.next
+            even = even.next
+            
+        odd.next = even_head
+            
+        return head
+            
