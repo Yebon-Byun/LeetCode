7f8d494b54247fd5203d09cd9439e6aceb447f1b
Yebon-Byun <96388412+Yebon-Byun@users.noreply.github.com>
2024-06-26 15:48:25 +0900

    Time: 136 ms (45.39%), Space: 31 MB (66.88%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -24,34 +24,58 @@
 # Approach 1: DFS, Recursion
 # Time Comp: O(n)
 # Space Comp: O(n)
-class Solution:
-    def goodNodes(self, root: TreeNode) -> int:
-        def dfs(node, max_so_far):
-            nonlocal num_good_nodes
-            if max_so_far <= node.val:
-                num_good_nodes += 1
-            if node.right:
-                dfs(node.right, max(node.val, max_so_far))
-            if node.left:
-                dfs(node.left, max(node.val, max_so_far))    
-        num_good_nodes = 0
-        dfs(root, float("-inf"))
-        return num_good_nodes
+# class Solution:
+#     def goodNodes(self, root: TreeNode) -> int:
+#         def dfs(node, max_so_far):
+#             nonlocal num_good_nodes
+#             if max_so_far <= node.val:
+#                 num_good_nodes += 1
+#             if node.right:
+#                 dfs(node.right, max(node.val, max_so_far))
+#             if node.left:
+#                 dfs(node.left, max(node.val, max_so_far))    
+#         num_good_nodes = 0
+#         dfs(root, float("-inf"))
+#         return num_good_nodes
 
 # Approach 2: DFS, Iterative
 # Time Comp: O(n)
 # Space Comp: O(n)
+# class Solution:
+#     def goodNodes(self, root: TreeNode) -> int:
+#         stack = [(root, float("-inf"))]
+#         num_good_nodes = 0
+#         while stack:
+#             node, max_so_far = stack.pop()
+#             print("node", node)
+#             print("max_so_far", max_so_far)
+#             if max_so_far <= node.val:
+#                 num_good_nodes += 1 
+#             if node.left:
+#                 stack.append((node.left, max(node.val, max_so_far)))
+#             if node.right:
+#                 stack.append((node.right, max(node.val, max_so_far)))
+#         return num_good_nodes
+
+# Approach 3: BFS
+# Time Comp: O(n)
+# Space Comp: O(n)
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        stack = [(root, float("-inf"))]
         num_good_nodes = 0
-        while stack:
-            node, max_so_far = stack.pop()
+        
+        # Use collections.deque for efficient popping
+        queue = deque([(root, float("-inf"))])
+        while queue:
+            node, max_so_far = queue.popleft()
             if max_so_far <= node.val:
-                num_good_nodes += 1 
-            if node.left:
-                stack.append((node.left, max(node.val, max_so_far)))
+                num_good_nodes += 1
             if node.right:
-                stack.append((node.right, max(node.val, max_so_far)))
+                queue.append((node.right, max(node.val, max_so_far)))
+            if node.left:
+                queue.append((node.left, max(node.val, max_so_far)))
+        
         return num_good_nodes
+        
+
         
