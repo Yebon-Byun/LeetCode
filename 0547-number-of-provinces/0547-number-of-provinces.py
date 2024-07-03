4c626c3efc992def946792cf1bd1da7f0d0cfc41
Yebon-Byun <96388412+Yebon-Byun@users.noreply.github.com>
2024-07-03 17:00:41 +0900

    Time: 180 ms (73.71%), Space: 17.6 MB (30.57%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,50 +1,56 @@
 # Approach 1: DFS
-# class Solution:
-#     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-#         n = len(isConnected)
-#         visited = [False] * n
-#         count = 0
-        
-#         if not isConnected:
-#             return 0
-        
-#         def dfs(current):
-#             for neighbor in range(n):
-#                 if isConnected[current][neighbor] == 1 and visited[neighbor] == False:
-#                     visited[neighbor] = True
-#                     dfs(neighbor)
-                    
-#         for idx in range(n):
-#             if visited[idx] == False:
-#                 count += 1
-#                 visited[idx] = True
-#                 dfs(idx)
-                
-#         return count
-    
-# Approach 2: BFS
+# Time Comp: O(n^2)
+# Space Comp: O(n)
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
         visited = [False] * n
         count = 0
         
-        def bfs(start):
-            queue = deque([start])
-            while queue:
-                current = queue.popleft()
-                for neighbor in range(n):
-                    if isConnected[current][neighbor] == 1 and not visited[neighbor]:
-                        visited[neighbor] = True
-                        queue.append(neighbor)
+        if not isConnected:
+            return 0
         
+        def dfs(current):
+            for neighbor in range(n):
+                if isConnected[current][neighbor] == 1 and visited[neighbor] == False:
+                    visited[neighbor] = True
+                    dfs(neighbor)
+                    
         for idx in range(n):
-            if not visited[idx]:
+            if visited[idx] == False:
                 count += 1
                 visited[idx] = True
-                bfs(idx)
+                dfs(idx)
                 
         return count
+    
+# Approach 2: BFS
+# Time Comp: O(n^2)
+# Space Comp: O(n)
+
+# class Solution:
+#     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+#         n = len(isConnected)
+#         visited = [False] * n
+#         count = 0
+        
+#         def bfs(start):
+#             queue = deque([start])
+#             while queue:
+#                 current = queue.popleft()
+#                 for neighbor in range(n):
+#                     if isConnected[current][neighbor] == 1 and not visited[neighbor]:
+#                         visited[neighbor] = True
+#                         queue.append(neighbor)
+        
+#         for idx in range(n):
+#             if not visited[idx]:
+#                 count += 1
+#                 visited[idx] = True
+#                 bfs(idx)
+                
+#         return count
 
 
             
